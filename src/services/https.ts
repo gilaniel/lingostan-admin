277f92f0248061768200afa2d51655282df5be7b
gila <gilaniel@yandex.ru>
2025-11-12 12:29:38 +0300

    change base url

--- src/services/https.ts
+++ src/services/https.ts
@@ -2,7 +2,7 @@ import { authStore } from "@/store/useAuthStore";
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
 });
 
